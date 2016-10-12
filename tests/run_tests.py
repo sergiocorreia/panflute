ddca4d7e93dd38cc578048cb1bd1184dcbf1610a
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-10-12 05:19:18 -0600

    Convert tabs to spaces

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -6,25 +6,25 @@ output_txt_benchmark = '../tests/1/benchmark.txt'
 output_txt_panflute = '../tests/1/panflute.txt'
 
 def empty_test(element, doc):
-	return
+    return
 
 def test_filter(element, doc):
-	if type(element)==pf.Header:
-		return []
-	if type(element)==pf.Str:
-		element.text = element.text + '!!'
-		return element
+    if type(element)==pf.Header:
+        return []
+    if type(element)==pf.Str:
+        element.text = element.text + '!!'
+        return element
 
 
 print('\nLoading JSON...')
 
 with open(input_fn, encoding='utf-8') as f:
-	doc = pf.load(f)
+    doc = pf.load(f)
 
 print('Dumping JSON...')
 with open(output_fn, mode='w', encoding='utf-8') as f:
-	pf.dump(doc, f)
-	f.write('\n')
+    pf.dump(doc, f)
+    f.write('\n')
 
 print(' - Done!')
 
@@ -32,10 +32,10 @@ print(' - Done!')
 print('\nComparing...')
 
 with open(input_fn, encoding='utf-8') as f:
-	input_data = f.read()
+    input_data = f.read()
 
 with open(output_fn, encoding='utf-8') as f:
-	output_data = f.read()
+    output_data = f.read()
 
 print('Are both files the same?')
 print(' - Length:', len(input_data) == len(output_data), len(input_data), len(output_data))
@@ -47,14 +47,14 @@ print(' - Done!')
 
 print(' - Dumping JSON...')
 with open(output_fn, mode='w', encoding='utf-8') as f:
-	pf.dump(doc, f)
-	f.write('\n')
+    pf.dump(doc, f)
+    f.write('\n')
 print(' - Done!')
 print(' - Comparing...')
 with open(input_fn, encoding='utf-8') as f:
-	input_data = f.read()
+    input_data = f.read()
 with open(output_fn, encoding='utf-8') as f:
-	output_data = f.read()
+    output_data = f.read()
 print(' - Are both files the same?')
 print('   - Length:', len(input_data) == len(output_data), len(input_data), len(output_data))
 print('   - Content:', input_data == output_data)
@@ -66,15 +66,15 @@ assert input_data == output_data
 
 print('Testing stringify()')
 with open(input_fn, encoding='utf-8') as f:
-	doc = pf.load(f)
+    doc = pf.load(f)
 ans = pf.stringify(doc)
 #print(repr(ans).encode('utf-8'))
 with open(output_txt_panflute, encoding='utf-8', mode='w') as f:
-	f.write(ans)
+    f.write(ans)
 
 import pandocfilters, json
 with open(input_fn, encoding='utf-8') as f:
-	doc = json.load(f)
+    doc = json.load(f)
 ans = pandocfilters.stringify(doc)
 with open(output_txt_benchmark, encoding='utf-8', mode='w') as f:
-	f.write(ans)
+    f.write(ans)
