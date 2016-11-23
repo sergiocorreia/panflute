f535c692d0784062ab2164b8129b96ca11c81276
Kolen Cheung <christian.kolen@gmail.com>
2016-11-23 00:22:09 -0800

    setup travis to always deploy on PyPI

--- setup.py
+++ setup.py
@@ -93,10 +93,10 @@ setup(
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    #extras_require={
+    extras_require={
     #    'dev': ['check-manifest'],
-    #    'test': ['coverage'],
-    #},
+        'test': ['shutilwhich', 'pandocfilters'],
+    },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
