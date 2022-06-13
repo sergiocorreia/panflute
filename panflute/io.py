720f1bb34c7c00d2170fc5ffb4e4c2f85acf4b16
Jacob Hall <email@jacobhall.net>
2022-06-13 12:25:36 -0400

    fix invalid escape sequence in panflute/io.py

--- panflute/io.py
+++ panflute/io.py
@@ -140,7 +140,7 @@ def toJSONFilter(*args, **kwargs):
     """
     Wrapper for :func:`.run_filter`, which calls :func:`.run_filters`
 
-    toJSONFilter(action, prepare=None, finalize=None, input_stream=None, output_stream=None, \*\*kwargs)
+    toJSONFilter(action, prepare=None, finalize=None, input_stream=None, output_stream=None, **kwargs)
     Receive a Pandoc document from stdin, apply the *action* function to each element, and write it back to stdout.
 
     See also :func:`.toJSONFilters`
